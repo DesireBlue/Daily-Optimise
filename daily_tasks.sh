bd4c8f0fdf7d0170f6de7303939cb97a3125599e
navid <Kalvandn@gmail.com>
2024-07-10 20:20:21 +0330

    Update daily_tasks.sh
    خالی کردن حافظه RAM سرور
    بهینه‌سازی عملکرد سرور
    پاکسازی کش‌ها و فضای دیسک
    افزایش سرعت عملکرد سرور
    اعلام عملکرد در ربات تلگرام

--- daily_tasks.sh
+++ daily_tasks.sh
@@ -2,6 +2,13 @@
 
 echo "Running daily tasks..."
 
+# Set your bot token and chat ID
+BOT_TOKEN="YOUR_BOT_TOKEN"
+CHAT_ID="YOUR_CHAT_ID"
+
+# Get server IP address
+SERVER_IP=$(hostname -I | cut -d' ' -f1)
+
 # Optimize server daily (replace with your optimization commands)
 echo "Optimizing server..."
 # Example: Run optimization commands here
@@ -41,6 +48,16 @@ sudo rm -rf /tmp/*
 # Display disk space
 echo "Disk space after cleaning:"
 df -h
+
+# Send Telegram message if BOT_TOKEN and CHAT_ID are set
+if [[ -n "${BOT_TOKEN}" && -n "${CHAT_ID}" ]]; then
+    echo "Sending Telegram message..."
+    message="Daily tasks completed. IP: ${SERVER_IP} *by : @the_nawid*"
+    curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d text="${message}" -d parse_mode="MarkdownV2"
+    echo "Telegram message sent."
+else
+    echo "Error: BOT_TOKEN or CHAT_ID is not set. Telegram message not sent."
+fi
 
 # Reboot server
 echo "Rebooting server..."
